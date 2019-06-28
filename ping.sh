d64290af0a24e815dc1fc250fa2bd82c21d19dfe
kamikat <kamikat@banana.moe>
2019-06-29 07:32:56 +0800

    add error on reading servers.lst

--- ping.sh
+++ ping.sh
@@ -10,14 +10,20 @@ structured_ping() {
 }
 
 PING_INTERVAL=${1:-60}
-
 shift
 
 while true; do
+  SERVER_LIST="$(cat $SERVER_FILE 2>/dev/null | cut -f1)"
+  if [ -z "$SERVER_LIST" ]; then
+    cat >&2 << ERROR
+ERROR: Cannot read server list from '$SERVER_FILE'.
+ERROR
+    exit -1
+  fi
   STAT_FILE=$STAT_DIR/$(date +%Y%m%d/%H%M%S)
   mkdir -p $(dirname $STAT_FILE)
-  echo "Pinging $(wc -l < $SERVER_FILE | tr -d ' ') server(s) for $PING_INTERVAL seconds..."
-  for SERVER_ADDR in $(cat $SERVER_FILE | cut -f1); do
+  echo "Pinging $(wc -l <<< "$SERVER_LIST" | tr -d ' ') server(s) for $PING_INTERVAL seconds..."
+  for SERVER_ADDR in $SERVER_LIST; do
     structured_ping $@ $SERVER_ADDR >>$STAT_FILE &
   done
   wait
