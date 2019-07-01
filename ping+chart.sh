3e643794b9d3f5e9fc296f60097feb1bd3b4b16c
kamikat <kamikat@banana.moe>
2019-07-01 11:03:47 +0800

    add batch analysis mode

--- ping+chart.sh
+++ ping+chart.sh
@@ -2,22 +2,18 @@
 
 source chart.sh
 
-to_script() {
-  awk "/^ping $IP_FILTER .*$/ {if (\$$COLUMN_ID!=\"\") print \$2 \"=\" \$$COLUMN_ID}" | xargs echo a
-}
-
 read_ping() {
   # read statistics store
   if [ ! -n "$NO_HISTORY" ]; then
-    while read STAT_FILE; do
-      cat $STAT_FILE | to_script
-    done < <(find statistics -type f | sort -r | tail -n+2 | head -n $(tput cols) | tac)
+    while read STAT_FILES; do
+      cat $STAT_FILES | awk "$AWK_PROG" | xargs echo a
+    done < <(find statistics -type f | sort -r | tail -n+2 | head -n $((HISTORY_COUNT * BATCH_SIZE)) | tac | xargs -n $BATCH_SIZE)
     echo g
   fi
   # read incremental log
   while read LINE; do
     if [[ "$LINE" =~ ^.*saved.\((.*)\).*$ ]]; then
-      cat ${BASH_REMATCH[1]} | to_script
+      cat ${BASH_REMATCH[1]} | awk "$AWK_PROG" | xargs echo a
       echo g
     fi
   done
@@ -25,6 +21,8 @@ read_ping() {
 
 INPUT_COMMAND="bash ping.sh"
 CHART_COMMAND="to_chart"
+HISTORY_COUNT="$(tput cols)"
+BATCH_SIZE=1
 
 while true; do
   case "$1" in
@@ -39,11 +37,21 @@ while true; do
     -q|--quiet)
       INPUT_COMMAND="echo"
       ;;
-    -c|--clear)
-      NO_HISTORY=1
-      ;;
     -r|--raw)
       CHART_COMMAND=cat
+      ;;
+    -b|--batch)
+      INPUT_COMMAND="echo"
+      BATCH_SIZE=$2
+      shift
+      ;;
+    -B|--batch-method)
+      BATCH_METHOD=$2
+      shift
+      ;;
+    -h|--history)
+      HISTORY_COUNT=$2
+      shift
       ;;
     *)
       break
@@ -88,5 +96,27 @@ else
 fi
 
 IP_FILTER=${2:-'.*'}
+
+get_prog() {
+  K="\$2"
+  V="\$$COLUMN_ID"
+  case $1 in
+    min)
+      ROW_STMT="min[$K] = (min[$K] != \"\" && min[$K] < $V ? min[$K] : $V)"
+      END_STMT="for (key in min) { print key \"=\" min[key] }"
+      ;;
+    max)
+      ROW_STMT="max[$K] = (max[$K] != \"\" && max[$K] > $V ? max[$K] : $V)"
+      END_STMT="for (key in max) { print key \"=\" max[key] }"
+      ;;
+    avg|average|mean)
+      ROW_STMT="sum[$K] += $V; count[$K]++;"
+      END_STMT="for (key in sum) { print key \"=\" (sum[key] / count[key]) }"
+      ;;
+  esac
+  echo "/^ping $IP_FILTER .*$/ { if ($V!=\"\") { $ROW_STMT } } END { $END_STMT }"
+}
+
+AWK_PROG=$(get_prog ${BATCH_METHOD:-max})
 
 $INPUT_COMMAND | read_ping | $CHART_COMMAND
